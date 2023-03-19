c30d652d5046cbf2560f2d3560745714ce880926
evelinaKrigere <124894388+evelinaKrigere@users.noreply.github.com>
2023-03-19 17:38:08 +0000

    1_meg

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ def parallel_processing(n, m, data):
     output = []
     # TODO: write the function for simulating parallel tasks, 
     # create the output pairs
+    beigu_t=[0]*n
+    for i in range(m):
+        min_t=min(beigu_t)
+        threads_sk=beigu_t.index(min_t)
+        output.append((threads_sk,min_t))
+
+        beigu_t[threads_sk]+=data[i]
 
     return output
 
@@ -13,17 +20,23 @@ def main():
     # first line - n and m
     # n - thread count 
     # m - job count
-    n = 0
-    m = 0
+    #n = 0
+    #m = 0
+    ievade=input()
+    if "I"in ievade:
+        n, m = map(int, input().split())
+        data = list(map(int, input().split()))
 
     # second line - data 
     # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
+    #data = []
 
     # TODO: create the function
     result = parallel_processing(n,m,data)
     
     # TODO: print out the results, each pair in it's own line
+    for threads_sk, sakuma_t in result:
+            print(threads_sk, sakuma_t)
 
 
 
